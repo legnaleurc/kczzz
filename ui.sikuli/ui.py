a44ba85424f67918716cab43de9060e557d90265
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-01-01 06:02:40 +0900

    Update UI.

--- ui.sikuli/ui.py
+++ ui.sikuli/ui.py
@@ -28,6 +28,7 @@ class Library(object):
         self._browser_alert_button_cancel = "browser_alert_button_cancel.png"
         self._flash_alert = "flash_alert.png"
         self._flash_alert_button_yes = "flash_alert_button_yes.png"
+        self._out_button = "out_button.png"
         self._system_screen_error = "system_screen_error.png"
         self._system_button_start = "system_button_start.png"
         
@@ -42,11 +43,19 @@ class Library(object):
         self._go_menu_button_long_trip = "go_menu_button_long_trip.png"
         self._long_trip_menu_button_ok = "long_trip_menu_button_ok.png"
         self._long_trip_menu_button_confirm = "long_trip_menu_button_confirm.png"
+        self._long_trip_menu_mission_2 = "long_trip_menu_mission_2.png"
         self._long_trip_menu_mission_3 = "long_trip_menu_mission_3.png"
         self._long_trip_menu_mission_5 = "long_trip_menu_mission_5.png"
+        self._long_trip_menu_mission_6 = "long_trip_menu_mission_6.png"
+        self._long_trip_menu_mission_9 = "long_trip_menu_mission_9.png"
+        self._long_trip_menu_mission_17 = "long_trip_menu_mission_17.png"
+        self._long_trip_menu_mission_20 = "long_trip_menu_mission_20.png"
+        self._long_trip_menu_stage_2 = "long_trip_menu_stage_2.png"
+        self._long_trip_menu_stage_3 = "long_trip_menu_stage.png"
         self._select_menu_label = "select_menu_label.png"
         self._select_menu_second_team = "select_menu_second_team.png"
         self._select_menu_third_team = "select_menu_third_team.png"
+        self._select_menu_forth_team = "select_menu_forth_team.png"
         self._select_menu_button_reload = "select_menu_button_reload.png"
         self._dock_menu_label = "dock_menu_label.png"
         self._dock_button_empty_slot = "dock_button_empty_slot.png"
@@ -70,6 +79,10 @@ class Library(object):
     @property
     def flashAlertButtonYes(self):
         return self._flash_alert_button_yes
+
+    @property
+    def outButton(self):
+        return self._out_button
 
     @property
     def systemScreenError(self):
@@ -124,12 +137,40 @@ class Library(object):
         return self._long_trip_menu_button_confirm
 
     @property
+    def longTripMenuMission2(self):
+        return self._long_trip_menu_mission_2
+
+    @property
     def longTripMenuMission3(self):
         return self._long_trip_menu_mission_3
 
     @property
     def longTripMenuMission5(self):
         return self._long_trip_menu_mission_5
+
+    @property
+    def longTripMenuMission6(self):
+        return self._long_trip_menu_mission_6
+
+    @property
+    def longTripMenuMission9(self):
+        return self._long_trip_menu_mission_9
+
+    @property
+    def longTripMenuMission17(self):
+        return self._long_trip_menu_mission_17
+
+    @property
+    def longTripMenuMission20(self):
+        return self._long_trip_menu_mission_20
+
+    @property
+    def longTripMenuStage2(self):
+        return self._long_trip_menu_stage_2
+
+    @property
+    def longTripMenuStage3(self):
+        return self._long_trip_menu_stage_3
 
     @property
     def selectMenuLabel(self):
@@ -142,6 +183,10 @@ class Library(object):
     @property
     def selectMenuThirdTeam(self):
         return self._select_menu_third_team
+
+    @property
+    def selectMenuForthTeam(self):
+        return self._select_menu_forth_team
 
     @property
     def selectMenuButtonReload(self):
