3c1201aaebf136576f418295f2a43d48493457ab
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-01-01 06:17:29 +0900

    Refactor UI.

--- ui.sikuli/ui.py
+++ ui.sikuli/ui.py
@@ -22,52 +22,37 @@ THE SOFTWARE.
 
 
 class Library(object):
-    
+
     def __init__(self):
         self._browser_alert = "browser_alert.png"
         self._browser_alert_button_cancel = "browser_alert_button_cancel.png"
         self._flash_alert = "flash_alert.png"
         self._flash_alert_button_yes = "flash_alert_button_yes.png"
-        self._out_button = "out_button.png"
-        self._system_screen_error = "system_screen_error.png"
-        self._system_button_start = "system_button_start.png"
-        
-        self._main_menu_button_go = "main_menu_button_go.png"
-        self._main_menu_button_repair = "main_menu_button_repair.png"
-        self._main_menu_button_reload = "main_menu_button_reload.png"
-        self._main_menu_label_long_trip_done = "main_menu_label_long_trip_done.png"
-        self._submenu_button_back = "submenu_button_back.png"
-        self._long_trip_screen_succeed = "long_trip_screen_succeed.png"
-        self._long_trip_screen_failed = "long_trip_screen_failed.png"
-        self._long_trip_screen_button_next = "long_trip_screen_button_next.png"
-        self._go_menu_button_long_trip = "go_menu_button_long_trip.png"
-        self._long_trip_menu_button_ok = "long_trip_menu_button_ok.png"
-        self._long_trip_menu_button_confirm = "long_trip_menu_button_confirm.png"
-        self._long_trip_menu_mission_2 = "long_trip_menu_mission_2.png"
-        self._long_trip_menu_mission_3 = "long_trip_menu_mission_3.png"
-        self._long_trip_menu_mission_5 = "long_trip_menu_mission_5.png"
-        self._long_trip_menu_mission_6 = "long_trip_menu_mission_6.png"
-        self._long_trip_menu_mission_9 = "long_trip_menu_mission_9.png"
-        self._long_trip_menu_mission_17 = "long_trip_menu_mission_17.png"
-        self._long_trip_menu_mission_20 = "long_trip_menu_mission_20.png"
-        self._long_trip_menu_stage_2 = "long_trip_menu_stage_2.png"
-        self._long_trip_menu_stage_3 = "long_trip_menu_stage.png"
-        self._select_menu_label = "select_menu_label.png"
-        self._select_menu_second_team = "select_menu_second_team.png"
-        self._select_menu_third_team = "select_menu_third_team.png"
-        self._select_menu_forth_team = "select_menu_forth_team.png"
-        self._select_menu_button_reload = "select_menu_button_reload.png"
-        self._dock_menu_label = "dock_menu_label.png"
-        self._dock_button_empty_slot = "dock_button_empty_slot.png"
-        self._dock_table_header = "dock_table_header.png"
-        self._dock_table_label_repairing = "dock_table_label_repairing.png"
-        self._dock_submenu_start_repair = "dock_submenu_start_repair.png"
-        self._dock_screen_confirm_repair_yes = "dock_screen_confirm_repair_yes.png"
+
+        self._long_trip_menu_mission = {
+            2: "long_trip_menu_mission_2.png",
+            3: "long_trip_menu_mission_3.png",
+            5: "long_trip_menu_mission_5.png",
+            6: "long_trip_menu_mission_6.png",
+            9: "long_trip_menu_mission_9.png",
+            17: "long_trip_menu_mission_17.png",
+            20: "long_trip_menu_mission_20.png",
+        }
+        self._long_trip_menu_stage = {
+            2: "long_trip_menu_stage_2.png",
+            3: "long_trip_menu_stage_3.png",
+        }
+
+        self._select_menu_team = {
+            2: "select_menu_second_team.png",
+            3: "select_menu_third_team.png",
+            4: "select_menu_forth_team.png",
+        }
 
     @property
     def browserAlert(self):
         return self._browser_alert
-    
+
     @property
     def browserAlertButtonCancel(self):
         return self._browser_alert_button_cancel
@@ -82,136 +67,97 @@ class Library(object):
 
     @property
     def outButton(self):
-        return self._out_button
+        return "out_button.png"
 
     @property
     def systemScreenError(self):
-        return self._system_screen_error
-    
+        return "system_screen_error.png"
+
     @property
     def systemButtonStart(self):
-        return self._system_button_start
-    
+        return "system_button_start.png"
+
     @property
     def mainMenuButtonGo(self):
-        return self._main_menu_button_go
-    
+        return "main_menu_button_go.png"
+
     @property
     def mainMenuButtonRepair(self):
-        return self._main_menu_button_repair
+        return "main_menu_button_repair.png"
 
     @property
     def mainMenuButtonReload(self):
-        return self._main_menu_button_reload
-    
+        return "main_menu_button_reload.png"
+
     @property
     def mainMenuLabelLongTripDone(self):
-        return self._main_menu_label_long_trip_done
-    
+        return "main_menu_label_long_trip_done.png"
+
     @property
     def submenuButtonBack(self):
-        return self._submenu_button_back
-    
+        return "submenu_button_back.png"
+
     @property
     def longTripScreenSucceed(self):
-        return self._long_trip_screen_succeed
-    
+        return "long_trip_screen_succeed.png"
+
     @property
     def longTripScreenFailed(self):
-        return self._long_trip_screen_failed
-    
+        return "long_trip_screen_failed.png"
+
     @property
     def longTripScreenButtonNext(self):
-        return self._long_trip_screen_button_next
+        return "long_trip_screen_button_next.png"
 
     @property
     def goMenuButtonLongTrip(self):
-        return self._go_menu_button_long_trip
+        return "go_menu_button_long_trip.png"
 
     @property
     def longTripMenuButtonOk(self):
-        return self._long_trip_menu_button_ok
+        return "long_trip_menu_button_ok.png"
 
     @property
     def longTripMenuButtonConfirm(self):
-        return self._long_trip_menu_button_confirm
+        return "long_trip_menu_button_confirm.png"
 
-    @property
-    def longTripMenuMission2(self):
-        return self._long_trip_menu_mission_2
+    def longTripMenuMission(self, id_):
+        return self._long_trip_menu_mission[id_]
 
-    @property
-    def longTripMenuMission3(self):
-        return self._long_trip_menu_mission_3
-
-    @property
-    def longTripMenuMission5(self):
-        return self._long_trip_menu_mission_5
-
-    @property
-    def longTripMenuMission6(self):
-        return self._long_trip_menu_mission_6
-
-    @property
-    def longTripMenuMission9(self):
-        return self._long_trip_menu_mission_9
-
-    @property
-    def longTripMenuMission17(self):
-        return self._long_trip_menu_mission_17
-
-    @property
-    def longTripMenuMission20(self):
-        return self._long_trip_menu_mission_20
-
-    @property
-    def longTripMenuStage2(self):
-        return self._long_trip_menu_stage_2
-
-    @property
-    def longTripMenuStage3(self):
-        return self._long_trip_menu_stage_3
+    def longTripMenuStage(self, id_):
+        return self._long_trip_menu_stage[id_]
 
     @property
     def selectMenuLabel(self):
-        return self._select_menu_label
+        return "select_menu_label.png"
 
-    @property
-    def selectMenuSecondTeam(self):
-        return self._select_menu_second_team
-
-    @property
-    def selectMenuThirdTeam(self):
-        return self._select_menu_third_team
-
-    @property
-    def selectMenuForthTeam(self):
-        return self._select_menu_forth_team
+    def selectMenuSecondTeam(self, id_):
+        return self._select_menu_team[id_]
 
     @property
     def selectMenuButtonReload(self):
-        return self._select_menu_button_reload
-    
+        return "select_menu_button_reload.png"
+
     @property
     def dockMenuLabel(self):
-        return self._dock_menu_label
-    
+        return "dock_menu_label.png"
+
     @property
     def dockButtonEmptySlot(self):
-        return self._dock_button_empty_slot
-    
+        return "dock_button_empty_slot.png"
+
     @property
     def dockTableHeader(self):
-        return self._dock_table_header
-    
+        return "dock_table_header.png"
+
     @property
     def dockTableLabelRepairing(self):
-        return self._dock_table_label_repairing
-    
+        return "dock_table_label_repairing.png"
+
     @property
     def dockSubmenuStartRepair(self):
-        return self._dock_submenu_start_repair
-    
+        return "dock_submenu_start_repair.png"
+
     @property
     def dockScreenConfirmRepairYes(self):
-        return self._dock_screen_confirm_repair_yes
+        return "dock_screen_confirm_repair_yes.png"
